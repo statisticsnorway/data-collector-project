d240c9ca65c173b37cef8f5ca7dc4c83425435a4
oranheim <oranheim@gmail.com>
2019-09-29 13:12:23 +0200

    Checkout and pull scripts

---
+++ checkout.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "git clone git@github.com:statisticsnorway/data-collector-project.git"
+git clone git@github.com:statisticsnorway/data-collector-project.git
+
+echo "git clone git@github.com:statisticsnorway/data-collector-api.git"
+git clone git@github.com:statisticsnorway/data-collector-api.git
+
+echo "git clone git@github.com:statisticsnorway/data-collector-content-store-rawdata-provider.git"
+git clone git@github.com:statisticsnorway/data-collector-content-store-rawdata-provider.git
+
+echo "git clone git@github.com:statisticsnorway/data-collector-core.git"
+git clone git@github.com:statisticsnorway/data-collector-core.git
+
+echo "git clone git@github.com:statisticsnorway/data-collector-testutils.git"
+git clone git@github.com:statisticsnorway/data-collector-testutils.git
+
+echo "git clone git@github.com:statisticsnorway/data-collector-javascript-processor.git"
+git clone git@github.com:statisticsnorway/data-collector-javascript-processor.git
+
+echo "git clone git@github.com:statisticsnorway/data-collector-server.git"
+git clone git@github.com:statisticsnorway/data-collector-server.git
