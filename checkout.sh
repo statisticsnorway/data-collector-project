d935506084322ce22f90b1a3d471d8a0fcb3d8c0
oranheim <oranheim@gmail.com>
2019-09-29 18:45:53 +0200

    Updated scripts

--- checkout.sh
+++ checkout.sh
@@ -3,17 +3,20 @@
 echo "git clone git@github.com:statisticsnorway/data-collector-api.git"
 git clone git@github.com:statisticsnorway/data-collector-api.git
 
-echo "git clone git@github.com:statisticsnorway/data-collector-content-store-rawdata-provider.git"
-git clone git@github.com:statisticsnorway/data-collector-content-store-rawdata-provider.git
-
-echo "git clone git@github.com:statisticsnorway/data-collector-core.git"
-git clone git@github.com:statisticsnorway/data-collector-core.git
+echo "git clone git@github.com:statisticsnorway/data-collector-server-base.git"
+git clone git@github.com:statisticsnorway/data-collector-server-base.git
 
 echo "git clone git@github.com:statisticsnorway/data-collector-testutils.git"
 git clone git@github.com:statisticsnorway/data-collector-testutils.git
 
+echo "git clone git@github.com:statisticsnorway/data-collector-content-store-rawdata-provider.git"
+git clone git@github.com:statisticsnorway/data-collector-content-store-rawdata-provider.git
+
 echo "git clone git@github.com:statisticsnorway/data-collector-javascript-processor.git"
 git clone git@github.com:statisticsnorway/data-collector-javascript-processor.git
+
+echo "git clone git@github.com:statisticsnorway/data-collector-core.git"
+git clone git@github.com:statisticsnorway/data-collector-core.git
 
 echo "git clone git@github.com:statisticsnorway/data-collector-server.git"
 git clone git@github.com:statisticsnorway/data-collector-server.git
--- pull.sh
+++ pull.sh
@@ -6,17 +6,21 @@ git -C ./ pull --rebase
 echo "git -C data-collector-api/ pull --rebase"
 git -C data-collector-api/ pull --rebase
 
-echo "git -C data-collector-content-store-rawdata-provider/ pull --rebase"
-git -C data-collector-content-store-rawdata-provider/ pull --rebase
-
-echo "git -C data-collector-core/ pull --rebase"
-git -C data-collector-core/ pull --rebase
+echo "git -C data-collector-server-base/ pull --rebase"
+git -C data-collector-server-base/ pull --rebase
 
 echo "git -C data-collector-testutils/ pull --rebase"
 git -C data-collector-testutils/ pull --rebase
 
+echo "git -C data-collector-content-store-rawdata-provider/ pull --rebase"
+git -C data-collector-content-store-rawdata-provider/ pull --rebase
+
 echo "git -C data-collector-javascript-processor/ pull --rebase"
 git -C data-collector-javascript-processor/ pull --rebase
 
+echo "git -C data-collector-core/ pull --rebase"
+git -C data-collector-core/ pull --rebase
+
 echo "git -C data-collector-server/ pull --rebase"
 git -C data-collector-server/ pull --rebase
+
